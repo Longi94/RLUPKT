632e93d01fcd6ea70f3a4208cf93df635128290d
Long Tran <lngtrn94@gmail.com>
2019-12-18 17:39:03 +0100

    use commandlineparser

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,14 +1,29 @@
 ﻿using RLUPKT.Core;
 using RLUPKT.Core.Encryption;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using CommandLine;
 
 namespace RLUPKT.ConsoleApp
 {
-
     internal class Program
     {
+        class Options
+        {
+            [Option('t', "tfc", Default = false,
+                HelpText = "Copy the tfc files from the input folder to the output folder.")]
+            public bool CopyTfc { get; set; }
+
+            [Option('i', "input", Required = true, HelpText = "Input folder containing the original upk packages.")]
+            public string Input { get; set; }
+
+            [Option('o', "output", Required = true,
+                HelpText = "Output folder where the decrypted upk packages will be placed.")]
+            public string Output { get; set; }
+        }
+
         private static void ProcessFile(string filePath, string outputPath)
         {
             using (var output = File.Open(outputPath, FileMode.Create))
@@ -47,51 +62,56 @@ namespace RLUPKT.ConsoleApp
 
                 //    }
                 //}
-
             }
         }
 
         private static void Main(string[] args)
         {
-            if (args.Count() < 4 || args[0] != "-f" || args[2] != "-o")
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
             {
-                Console.WriteLine("Usage: RLUPKTool -f <folder with packages> -o <decrypted folder>");
-                return;
-            }
+                if (o.Input.Equals(o.Output))
+                {
+                    Console.WriteLine("Input and output must not be the same");
+                    return;
+                }
 
-            var inputFolder = args[1];
-            var outputFolder = args[3];
-            Console.WriteLine(inputFolder);
-            Console.WriteLine(outputFolder);
-            foreach (var file in Directory.EnumerateFiles(inputFolder, "*.upk"))
-            {
-                if (file.EndsWith("_decrypted.upk"))
+                var inputFolder = o.Input;
+                var outputFolder = o.Output;
+                Console.WriteLine(inputFolder);
+                Console.WriteLine(outputFolder);
+                foreach (var file in Directory.EnumerateFiles(inputFolder, "*.upk"))
                 {
-                    Console.Error.WriteLine("File is already decrypted.");
-                    continue;
+                    if (file.EndsWith("_decrypted.upk"))
+                    {
+                        Console.Error.WriteLine("File is already decrypted.");
+                        continue;
+                    }
+
+                    var inputFileName = Path.GetFileNameWithoutExtension(file);
+                    var outputFilePath = Path.Combine(outputFolder, inputFileName + "_decrypted.upk");
+                    new FileInfo(outputFilePath).Directory.Create();
+                    //Console.WriteLine($"Processing: {inputFileName}");
+                    try
+                    {
+                        ProcessFile(file, outputFilePath);
+                    }
+                    catch (InvalidDataException e)
+                    {
+                        Console.WriteLine("Exception caught: {0}", e);
+                    }
+                    catch (OutOfMemoryException e)
+                    {
+                        Console.WriteLine("Exception caught: {0}", e);
+                    }
                 }
-                var inputFileName = Path.GetFileNameWithoutExtension(file);
-                var outputFilePath = Path.Combine(outputFolder, inputFileName + "_decrypted.upk");
-                new FileInfo(outputFilePath).Directory.Create();
-                //Console.WriteLine($"Processing: {inputFileName}");
-                try
+
+                for (int i = 0; i < AESKeys.KeyList.Count; i++)
                 {
-                    ProcessFile(file, outputFilePath);
+                    Console.WriteLine("Key{0} got used {1} times", i + 1, AESKeys.KeyListSuccessCount[i]);
                 }
-                catch (InvalidDataException e)
-                {
-                    Console.WriteLine("Exception caught: {0}", e);
-                }
-                catch (OutOfMemoryException e)
-                {
-                    Console.WriteLine("Exception caught: {0}", e);
-                }
-            }
-            for (int i = 0; i < AESKeys.KeyList.Count; i++)
-            {
-                Console.WriteLine("Key{0} got used {1} times", i + 1, AESKeys.KeyListSuccessCount[i]);
-            }
+
                 Console.WriteLine("Finished!");
+            });
         }
     }
 }
